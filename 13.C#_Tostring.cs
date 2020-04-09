23e3e547c4e19a8f5368ce38f38a4e8d4869b3ee
Songsonge <62552513+Songsonge@users.noreply.github.com>
2020-04-10 00:22:18 +0900

    Create 13.C#_Tostring.cs

---
+++ 13.C#_Tostring.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            string s = "asdf ss";
+            string s2 = a.ToString();
+            Console.WriteLine(s2);
+
+        }
+    }
+}
